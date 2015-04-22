bd707362bac2b78cd8d75cf59ac26a7e43ec09b8
nanfengbobo <Seven7.2011@foxmail.com>
2015-04-22 22:08:10 +0800

    remove

--- extjs5-starter/src/main/webapp/app/StringUtils.js
+++
@@ -1,9 +0,0 @@
-Ext.define('App.StringUtils', {
-	singleton : true,
-	isBlank : function(value){
-		return Ext.isEmpty(value ? (Ext.isString(value) ? Ext.String.trim(value) : Ext.String.trim(Ext.toString(value))):value);
-	},
-	isNotBlank : function(value){
-		return !this.isBlank(value);
-	}
-});
